4cc314073461e01c3b62e9d8a3d905f3328e4f00
Tim Terwijn <tim@tterwijn.nl>
2021-12-19 16:02:13 +0100

    Done with testing experiments part1

--- Afstudeer-oefenopdrachten/Bank/BankAccount.cs
+++ Afstudeer-oefenopdrachten/Bank/BankAccount.cs
@@ -1,12 +1,14 @@
 using System;
 
-namespace BankAccountNS
-{
+namespace BankAccountNS{
     /// <summary>
     /// Bank account demo class.
     /// </summary>
-    public class BankAccount
-    {
+    public class BankAccount{
+
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
+
         private readonly string m_customerName;
         private double m_balance;
 
@@ -27,14 +29,14 @@ namespace BankAccountNS
 
         public void Debit(double amount){
             if (amount > m_balance){
-                throw new ArgumentOutOfRangeException("amount");
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
             }
 
             if (amount < 0){
-                throw new ArgumentOutOfRangeException("amount");
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
             }
 
-            m_balance += amount; // intentionally incorrect code
+            m_balance -= amount;
         }
 
         public void Credit(double amount){
---
+++ Afstudeer-oefenopdrachten/BankTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f5b21d2-ca31-4fa0-a26b-3b1357d42980")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
